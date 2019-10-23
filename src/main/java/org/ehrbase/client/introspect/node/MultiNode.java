b14725343346264f7f6164868c8b08726729eac4
Stefan Spiska <s.spiska@symeda.de>
2019-10-23 15:17:06 +0200

    #ehrbase/project_management#18 m-occurr in Introspect
    support for multi occurrence in TemplateIntrospect

---
+++ src/main/java/org/ehrbase/client/introspect/node/MultiNode.java
@@ -0,0 +1,28 @@
+package org.ehrbase.client.introspect.node;
+
+import java.util.Map;
+
+public class MultiNode implements Node {
+    protected final String name;
+    protected final boolean multi;
+    protected final Map<String, Node> children;
+
+    public MultiNode(String name, boolean multi, Map<String, Node> children) {
+        this.name = name;
+        this.multi = multi;
+        this.children = children;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public boolean isMulti() {
+        return multi;
+    }
+
+    public Map<String, Node> getChildren() {
+        return children;
+    }
+}
