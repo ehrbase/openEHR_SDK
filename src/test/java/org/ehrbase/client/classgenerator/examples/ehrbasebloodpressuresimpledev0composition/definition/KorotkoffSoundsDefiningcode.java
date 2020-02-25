9829ce6c5714abdd89db5fa3073b607b745f698b
Stefan Spiska <s.spiska@symeda.de>
2020-02-25 11:10:01 +0100

    ehrbase/project_management#113: updated generated classes

---
+++ src/test/java/org/ehrbase/client/classgenerator/examples/ehrbasebloodpressuresimpledev0composition/definition/KorotkoffSoundsDefiningcode.java
@@ -0,0 +1,40 @@
+package org.ehrbase.client.classgenerator.examples.ehrbasebloodpressuresimpledev0composition.definition;
+
+import org.ehrbase.client.classgenerator.EnumValueSet;
+
+public enum KorotkoffSoundsDefiningcode implements EnumValueSet {
+    FIFTHSOUND("Fifth sound", "The fifth Korotkoff sound is identified by absence of sounds as the cuff pressure drops below the diastolic blood pressure", "local", "at1012"),
+
+    FOURTHSOUND("Fourth sound", "The fourth Korotkoff sound is identified as an abrupt muffling of sounds", "local", "at1011");
+
+    private String value;
+
+    private String description;
+
+    private String terminologyId;
+
+    private String code;
+
+    KorotkoffSoundsDefiningcode(String value, String description, String terminologyId, String code) {
+        this.value = value;
+        this.description = description;
+        this.terminologyId = terminologyId;
+        this.code = code;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getTerminologyId() {
+        return this.terminologyId;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+}
