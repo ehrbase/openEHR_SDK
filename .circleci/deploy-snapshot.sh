1a8940b73ff3f2fa1698479244b5df4e14b5c9b9
Wlad Wagner <38438046+testautomation@users.noreply.github.com>
2020-06-29 18:12:50 +0200

    foo bar major bla release baz

--- .circleci/deploy-snapshot.sh
+++ .circleci/deploy-snapshot.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
+# Check if two substr (i.e. 'major' AND 'release') are contained in $last_commit message.
+# NOTE: this is a workaround for the non existing AND operator in bash's CASE
+#       using double negation with OR as described in
+#       https://unix.stackexchange.com/questions/549165/and-operator-in-case-statement#549316
 shopt -s extglob
 case $last_commit in
-    # Check if the two substr 'major' AND 'release'
-    # are contained in $last_commit message.
-    # NOTE: this is a workaround for the non existing AND operator in bash's CASE
-    #       using double negation with OR as described in
-    #       https://unix.stackexchange.com/questions/549165/and-operator-in-case-statement#549316
     ( !(!(*"major"*)|!(*"release"*)) )
         echo "deploy major release"
         # exit 0
