6faaa921540c150a9d033135457a0bfa396945a0
Wlad Wagner <38438046+testautomation@users.noreply.github.com>
2020-06-29 17:30:57 +0200

    moved deploy snapsht login into sh script

---
+++ .circleci/deploy-snapshot.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+case $last_commit in
+    # Check if the two substr 'major' AND 'release'
+    # are contained in $last_commit message.
+    # NOTE: this is a workaround for the non existing AND operator in bash's CASE
+    #       using double negation with OR as described in
+    #       https://unix.stackexchange.com/questions/549165/and-operator-in-case-statement#549316
+    ( !(!(*"major"*)|!(*"release"*)) )
+        echo "deploy major release"
+        # exit 0
+        ;;
+    ( !(!(*"minor"*)|!(*"release"*)) )
+        echo "deploy minor release"
+        # exit 0
+        ;;
+    ( !(!(*"patch"*)|!(*"release"*)) )
+        echo "deploy patch release"
+        # exit 0
+        ;;
+    *)
+        echo "do nothing"
+        # exit 0
+        ;;
+esac
