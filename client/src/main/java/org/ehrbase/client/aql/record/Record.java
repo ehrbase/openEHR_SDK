45c074c8a7a692a08b3e4a6e5bc42dbcb28bb607
Stefan Spiska <s.spiska@symeda.de>
2021-01-11 15:21:52 +0100

    fix  sonar issue
    see https://github.com/ehrbase/project_management/issues/421

--- client/src/main/java/org/ehrbase/client/aql/record/Record.java
+++ client/src/main/java/org/ehrbase/client/aql/record/Record.java
@@ -23,12 +23,11 @@ import org.ehrbase.client.aql.field.AqlField;
 
 public interface Record {
 
-    AqlField<?> field(int index);
+  AqlField<Object> field(int index);
 
-    AqlField<?>[] fields();
+  AqlField<Object>[] fields();
 
-    Object value(int index);
+  Object value(int index);
 
-    Object[] values();
-
+  Object[] values();
 }
