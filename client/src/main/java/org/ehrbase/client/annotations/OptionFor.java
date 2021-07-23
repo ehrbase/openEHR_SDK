05f07f523a146d84393e0f97ee15159abc5738b5
stefan.spiska <stefan.spiska@vitagroup.ag>
2021-07-23 12:24:55 +0200

    added  @Inherited annotation to dtos annotations
    see https://github.com/ehrbase/openEHR_SDK/issues/243

--- client/src/main/java/org/ehrbase/client/annotations/OptionFor.java
+++ client/src/main/java/org/ehrbase/client/annotations/OptionFor.java
@@ -1,12 +1,10 @@
 package org.ehrbase.client.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(value = {ElementType.TYPE})
+@Inherited
 public @interface OptionFor {
     public static final String VALUE = "value";
 
