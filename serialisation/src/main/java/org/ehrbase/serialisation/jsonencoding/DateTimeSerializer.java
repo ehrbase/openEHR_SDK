b8ced13cd7b87f44283ad88ab989062e0b723431
stefan.spiska <stefan.spiska@vitagroup.ag>
2022-02-25 15:35:26 +0100

    fix dv_date_time format
    see https://jira.vitagroup.ag/browse/CDR-232

--- serialisation/src/main/java/org/ehrbase/serialisation/jsonencoding/DateTimeSerializer.java
+++ serialisation/src/main/java/org/ehrbase/serialisation/jsonencoding/DateTimeSerializer.java
@@ -23,28 +23,53 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
 import com.nedap.archie.rm.datavalues.quantity.datetime.DvDateTime;
+import com.nedap.archie.rm.datavalues.quantity.datetime.DvTemporal;
 import org.apache.commons.collections4.CollectionUtils;
 
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.format.DecimalStyle;
+import java.time.temporal.ChronoField;
 
 import static com.nedap.archie.datetime.DateTimeSerializerFormatters.ISO_8601_DATE;
-import static com.nedap.archie.datetime.DateTimeSerializerFormatters.ISO_8601_TIME;
+
+
 
 /**
  * custom serializer for DvDateTime instance with dot delimiter instead of comma
  */
 public class DateTimeSerializer extends JsonSerializer<DvDateTime> {
 
-    public static final DateTimeFormatter ISO_8601_DATE_TIME = new DateTimeFormatterBuilder()
-            .parseCaseInsensitive()
-            .append(ISO_8601_DATE)
-            .appendLiteral('T')
-            .append(ISO_8601_TIME)
-            .toFormatter()
-            .withDecimalStyle(DecimalStyle.STANDARD.withDecimalSeparator('.'));
+  static final DateTimeFormatter ISO_8601_TIME =
+      (new DateTimeFormatterBuilder())
+          .parseCaseInsensitive()
+          .appendValue(ChronoField.HOUR_OF_DAY, 2)
+          .optionalStart()
+          .appendLiteral(':')
+          .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+          .optionalStart()
+          .appendLiteral(':')
+          .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+          .optionalStart()
+          .appendFraction(ChronoField.NANO_OF_SECOND, 3, 9, true)
+          .optionalEnd()
+          .optionalEnd()
+          .optionalEnd()
+          .optionalStart()
+          .appendOffsetId()
+          .optionalEnd()
+          .toFormatter()
+          .withDecimalStyle(DecimalStyle.STANDARD.withDecimalSeparator(','));
+
+  public static final DateTimeFormatter ISO_8601_DATE_TIME =
+      new DateTimeFormatterBuilder()
+          .parseCaseInsensitive()
+          .append(ISO_8601_DATE)
+          .appendLiteral('T')
+          .append(ISO_8601_TIME)
+          .toFormatter()
+          .withDecimalStyle(DecimalStyle.STANDARD.withDecimalSeparator('.'));
 
     @Override
     public void serialize(DvDateTime dvDateTime, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
@@ -68,26 +93,31 @@ public class DateTimeSerializer extends JsonSerializer<DvDateTime> {
     jsonGenerator.writeStartObject();
     jsonGenerator.writeStringField("_type", "DV_DATE_TIME");
     jsonGenerator.writeStringField("value", ISO_8601_DATE_TIME.format(dvDateTime.getValue()));
-    if (dvDateTime.getNormalStatus() != null) {
-      jsonGenerator.writeFieldName("normal_status");
-      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvDateTime.getNormalStatus()));
-    }
-    if (dvDateTime.getNormalRange() != null) {
-      jsonGenerator.writeFieldName("normal_range");
-      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvDateTime.getNormalRange()));
-    }
-    if (!CollectionUtils.isEmpty(dvDateTime.getOtherReferenceRanges())) {
-      jsonGenerator.writeFieldName("other_reference_ranges");
-      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvDateTime.getOtherReferenceRanges()));
-    }
-    if (dvDateTime.getMagnitudeStatus() != null) {
-      jsonGenerator.writeFieldName("magnitude_status");
-      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvDateTime.getMagnitudeStatus()));
-    }
-    if (dvDateTime.getNormalStatus() != null) {
-      jsonGenerator.writeFieldName("accuracy");
-      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvDateTime.getAccuracy()));
-    }
+    writeDvTemporal(jsonGenerator, dvDateTime, mapper);
     jsonGenerator.writeEndObject();
+  }
+
+  static void writeDvTemporal(
+      JsonGenerator jsonGenerator, DvTemporal dvTemporal, ObjectMapper mapper) throws IOException {
+    if (dvTemporal.getNormalStatus() != null) {
+      jsonGenerator.writeFieldName("normal_status");
+      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvTemporal.getNormalStatus()));
+    }
+    if (dvTemporal.getNormalRange() != null) {
+      jsonGenerator.writeFieldName("normal_range");
+      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvTemporal.getNormalRange()));
+    }
+    if (!CollectionUtils.isEmpty(dvTemporal.getOtherReferenceRanges())) {
+      jsonGenerator.writeFieldName("other_reference_ranges");
+      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvTemporal.getOtherReferenceRanges()));
+    }
+    if (dvTemporal.getMagnitudeStatus() != null) {
+      jsonGenerator.writeFieldName("magnitude_status");
+      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvTemporal.getMagnitudeStatus()));
+    }
+    if (dvTemporal.getNormalStatus() != null) {
+      jsonGenerator.writeFieldName("accuracy");
+      jsonGenerator.writeRawValue(mapper.writeValueAsString(dvTemporal.getAccuracy()));
+    }
   }
 }
