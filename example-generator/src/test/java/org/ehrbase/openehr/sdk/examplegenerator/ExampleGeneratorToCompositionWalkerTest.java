1aaf1cef8fe2807f671c40fa9be89d8e5fc9aa2f
Holger Reise <holger.reise@vitagroup.ag>
2022-04-01 15:23:19 +0200

    #14 add license headers

--- example-generator/src/test/java/org/ehrbase/openehr/sdk/examplegenerator/ExampleGeneratorToCompositionWalkerTest.java
+++ example-generator/src/test/java/org/ehrbase/openehr/sdk/examplegenerator/ExampleGeneratorToCompositionWalkerTest.java
@@ -1,9 +1,22 @@
+/*
+ * Copyright (c) 2022. vitasystems GmbH and Hannover Medical School.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.ehrbase.openehr.sdk.examplegenerator;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.nedap.archie.rm.composition.Composition;
-import org.apache.xmlbeans.XmlException;
 import org.ehrbase.building.webtemplateskeletnbuilder.WebTemplateSkeletonBuilder;
 import org.ehrbase.client.classgenerator.shareddefinition.Category;
 import org.ehrbase.client.classgenerator.shareddefinition.Language;
@@ -18,16 +31,11 @@ import org.ehrbase.serialisation.walker.defaultvalues.DefaultValues;
 import org.ehrbase.test_data.operationaltemplate.OperationalTemplateTestData;
 import org.ehrbase.validation.CompositionValidator;
 import org.ehrbase.validation.ConstraintViolation;
-import org.ehrbase.webtemplate.filter.Filter;
-import org.ehrbase.webtemplate.model.FilteredWebTemplate;
 import org.ehrbase.webtemplate.model.WebTemplate;
 import org.ehrbase.webtemplate.model.WebTemplateNode;
-import org.ehrbase.webtemplate.templateprovider.TemplateProvider;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
-import org.openehr.schemas.v1.OPERATIONALTEMPLATE;
-import org.openehr.schemas.v1.TemplateDocument;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -37,9 +45,7 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.OffsetDateTime;
 import java.util.*;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
