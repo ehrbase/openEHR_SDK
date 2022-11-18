c3112c5b33634e248bcd57d6690289b123cf2bdd
Dmitry Tulba <dtulba@reloadly.com>
2022-11-18 14:20:27 +0200

    apply spotless

--- response-dto/src/main/java/org/ehrbase/response/openehr/ContributionCreateDto.java
+++ response-dto/src/main/java/org/ehrbase/response/openehr/ContributionCreateDto.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2022 vitasystems GmbH and Hannover Medical School.
+ *
+ * This file is part of project openEHR_SDK
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.ehrbase.response.openehr;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -7,17 +24,15 @@ import com.nedap.archie.rm.RMObject;
 import com.nedap.archie.rm.changecontrol.OriginalVersion;
 import com.nedap.archie.rm.generic.AuditDetails;
 import com.nedap.archie.rm.support.identification.HierObjectId;
-
+import java.util.ArrayList;
+import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlType;
-import java.util.ArrayList;
-import java.util.List;
 
 @XmlType(
         name = "CONTRIBUTION",
-        propOrder = {"uid", "versions", "audit"}
-)
+        propOrder = {"uid", "versions", "audit"})
 @XmlAccessorType(XmlAccessType.FIELD)
 public class ContributionCreateDto extends RMObject {
 
