5cba04bce86e8a6fec6592dd3db7fed71063ed30
vinzenz.mueller <vinzenz.mueller@vitagroup.ag>
2023-07-03 11:44:40 +0200

    CDR-934 rename test

--- web-template/src/test/java/org/ehrbase/openehr/sdk/webtemplate/path/flat/FlatPathParserTest.java
+++ web-template/src/test/java/org/ehrbase/openehr/sdk/webtemplate/path/flat/FlatPathParserTest.java
@@ -21,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
-public class EnhancedAqlPathParserTest {
+public class FlatPathParserTest {
 
     @Test
     public void parse() {
