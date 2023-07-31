2d67bf7bc42ecf2936a4fcdcbc11b6612c7777e4
Holger Reise <holger.reise@vitagroup.ag>
2023-07-31 12:48:46 +0200

    removed duplicate "Virologischer Befund"

--- test-data/src/main/java/org/ehrbase/openehr/sdk/test_data/operationaltemplate/OperationalTemplateTestData.java
+++ test-data/src/main/java/org/ehrbase/openehr/sdk/test_data/operationaltemplate/OperationalTemplateTestData.java
@@ -70,7 +70,6 @@ public enum OperationalTemplateTestData {
             "Deterioriation assessment ",
             "EREACT - Deterioriation assessment.v0.opt",
             "EREACT - Deterioriation assessment.v0"),
-    VIROLOGISCHER_BEFUND("Virologischer Befund", "Virologischer_Befund.opt", "Virologischer Befund"),
     LABOR_BEFUND("Labor Befund", "laborbefund.opt", "Laborbefund"),
     GECCO_DIAGNOSE("example with snomed terminologies", "GECCO_Diagnose.opt", "GECCO_Diagnose"),
     MINIMAL_ACTION_2("Minimal Action 2", "minimal_action2.opt", "Minimal action 2"),
