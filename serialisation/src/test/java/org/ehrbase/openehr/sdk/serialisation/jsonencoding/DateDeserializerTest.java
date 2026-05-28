6d0ab90165f425f931dc424e9374e4b5b23b2787
Holger Reise <holger.reise@vitagroup.ag>
2026-05-28 21:36:30 +0200

    CDR-541 fix DateDeserializerTest

--- serialisation/src/test/java/org/ehrbase/openehr/sdk/serialisation/jsonencoding/DateDeserializerTest.java
+++ serialisation/src/test/java/org/ehrbase/openehr/sdk/serialisation/jsonencoding/DateDeserializerTest.java
@@ -31,6 +31,7 @@ import java.io.IOException;
 import java.time.Year;
 import java.util.List;
 import org.ehrbase.openehr.sdk.util.OpenEHRDateTimeParseUtils;
+import org.ehrbase.openehr.sdk.util.PartialDate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -120,7 +121,7 @@ class DateDeserializerTest {
         ALL_ATTRIBUTES_SET_FULL_RESOLUTION(
                 new DvDate(
                         List.of(new ReferenceRange<>(new DvText("meaning"), new DvInterval<DvDate>(null, null))),
-                        new DvInterval<>(new DvDate(Year.of(2023)), null),
+                        new DvInterval<>(new DvDate(PartialDate.of(Year.of(2023))), null),
                         new CodePhrase(),
                         "magStat",
                         new DvDuration(),
